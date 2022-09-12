eb612a7b14758411edd581b35c4a689c240737ba
Denver Jackson <denvergjackson@gmail.com>
2022-09-12 18:53:11 +0700

    Puppeter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 
 (async function main(){
-	try {
+	try { 
 		
 		const browser = await puppeteer.launch({headless: false});
 		const page = await browser.newPage();
